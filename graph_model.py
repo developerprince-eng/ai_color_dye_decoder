f5e6049281e8ead26c8b09369dae2e815e6bbfb9
DeveloperPrince <prince.maposa@ciibo.com>
2020-04-19 14:20:58 +0200

    remodel graph image

--- graph_model.py
+++ graph_model.py
@@ -6,16 +6,12 @@ from keras.utils import plot_model
 
 def main():
     model = Sequential()
-    model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(28, 28, 1)))
-    model.add(Conv2D(64, (3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-    model.add(Flatten())
-    model.add(Dense(128, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(1, activation='softmax'))                                                                                                                                                                                                                        
+    model.add(Dense(1, input_dim=x_dim, init='uniform', activation='relu'))
+    model.add(Dense(4, init='uniform', activation='relu'))
+    model.add(Dense(8, init='uniform', activation='sigmoid'))
+    model.add(Dense(5, init='uniform', activation='sigmoid'))                                                                                                                                                                                                                       
     # Compile model
-    model.compile(loss='mean_squared_error', optimizer='adam',
+    model.compile(loss='binary_crossentropy', optimizer='adam',
     metrics=['accuracy'])
     #Model Summary
     model.summary()
